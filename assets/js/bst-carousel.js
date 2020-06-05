2f4ab50bebb1a15dda53a2917774c8c1cf52faf8
Daniela <daniela.galue10@gmail.com>
2020-06-05 00:44:48 -0400

    Home - Sections

---
+++ assets/js/bst-carousel.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.carousel-item', '.show-neighbors').each(function(){
+        var next = $(this).next();
+        if (! next.length) {
+          next = $(this).siblings(':first');
+        }
+        nextNeighbor=next.children(':first-child').clone();
+        nextNeighbor.addClass("neighbor-next");
+        nextNeighbor.appendTo($(this));
+    
+      }).each(function(){
+        var prev = $(this).prev();
+        if (! prev.length) {
+          prev = $(this).siblings(':last');
+        }
+        prevNeighbor=prev.children(':nth-last-child(2)').clone();
+        prevNeighbor.addClass("neighbor-prev");
+        prevNeighbor.prependTo($(this));
+    });
+    $('.carousel').carousel('pause');
+});
