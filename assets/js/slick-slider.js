8f0b7199fd3aa53d67b10f12c02e4ccec35a7d01
Daniela <daniela.galue10@gmail.com>
2020-06-07 23:34:07 -0400

    Home

--- assets/js/slick-slider.js
+++ assets/js/slick-slider.js
@@ -34,51 +34,20 @@ function initSlickSlider(){
       {
         breakpoint: 767,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          arrows:true
           /*centerMode: true*/
         }
       },
       {
-        breakpoint: 480,
+        breakpoint: 576,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1
+          slidesToScroll: 1,
+          arrows:true
         }
       }
     ]
   });
-
-  // $('.responsive').slick({
-  //   dots: true,
-  //   infinite: false,
-  //   speed: 300,
-  //   slidesToShow: 3,
-  //   slidesToScroll: 3,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         slidesToShow: 3,
-  //         slidesToScroll: 3,
-  //         infinite: true,
-  //         dots: true
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 767,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         slidesToShow: 1,
-  //         slidesToScroll: 1
-  //       }
-  //     }
-  //   ]
-  // });
 }
