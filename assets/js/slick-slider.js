89d3406f5caaca3b4b8c2eb77beedf08f20ed112
Daniela <daniela.galue10@gmail.com>
2020-06-09 01:26:47 -0400

    About - sections

--- assets/js/slick-slider.js
+++ assets/js/slick-slider.js
@@ -15,39 +15,22 @@ function initSlickSlider(){
     slidesToScroll: 3,
     focusOnSelect: true,
     asNavFor: '.slider-for',
-    /*slidesToShow: 3,
+    slidesToShow: 3,
     slidesToScroll: 3,
     asNavFor: '.slider-for',
     dots: false,
-    centerMode: true,
+    /*centerMode: true,
     focusOnSelect: true,*/
      responsive: [
       {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
         breakpoint: 767,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          arrows:true
-          /*centerMode: true*/
-        }
-      },
-      {
-        breakpoint: 576,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
           arrows:true
+          /*centerMode: true*/
         }
-      }
+      },
     ]
   });
 }
