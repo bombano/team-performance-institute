8b07ed1355b9e53d50bc8a232122772ce5357387
Daniela <daniela.galue10@gmail.com>
2020-06-11 02:23:53 -0400

    Services - Featured course section I

--- assets/js/owl-carousel.js
+++ assets/js/owl-carousel.js
@@ -15,4 +15,20 @@ function initOwlCarousel(){
             },
         }
     });
+    $('#carousel-featured-courses').owlCarousel({
+        loop:true,
+        margin:30,
+        navText: ['','<i class="fas fa-chevron-right"></i>'],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            992:{
+                items:3,
+                nav:true,
+            },
+        }
+    });
 }
