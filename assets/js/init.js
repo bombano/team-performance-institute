b778f90be8c94bd4ed12d5a5d302a22896fed076
Daniela <daniela.galue10@gmail.com>
2020-06-12 02:11:20 -0400

    Home - Leaders (Not finished yet)

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,5 +1,26 @@
 $(function () {
     initNavbarFixed();
-    initSlickSlider();
     initOwlCarousel();
+    initTeamSlider();
 });
+
+
+function initTeamSlider(){
+   $(document).on("click",".slide-nav", function(){
+       var currentNav=$(this);
+       var currentIndex=currentNav.attr("data-index");
+       var currentSlide=$(".team-slider.slider").find("[data-index='"+currentIndex+"']"); 
+        
+       if(!currentNav.hasClass("active")){
+            var activeNav=$(".slide-nav.active");
+            var activeIndex=activeNav.attr("data-index");
+            var activeSlide=$(".slider").find("[data-index='"+activeIndex+"']");
+
+            activeNav.removeClass("active");
+            activeSlide.removeClass("active");
+
+            currentNav.addClass("active");
+            currentSlide.addClass("active"); 
+        }
+   });
+}
